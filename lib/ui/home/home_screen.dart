c016c90d298ec0312e7be14bfa9ee51a30ff40d9
rayana.almeida <rayana.almeida@icomp.ufam.edu.br>
2025-04-17 10:34:05 +0000

    feat: add HomeScreen body

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -12,6 +12,21 @@ class HomeScreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
         ],
       ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          spacing: 32.0,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(child: Image.asset('assets/NhamiNhami.png', width: 200)),
+            Text("Boas Vindas!"),
+            TextFormField(),
+            Text("Escolha por categoria"),
+            Image.asset('assets/banners/banner_promo.png'),
+            Text("Bem avaliados"),
+          ],
+        ),
+      ),
     );
   }
 }
